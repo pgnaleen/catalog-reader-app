74f68659f4081592400b8b4bc12a23706b3d51a4
Nalin Kularathna <pgnaleen@gmail.com>
2021-01-02 14:16:51 +0000

    adding app.py Dockerfile cloudbuild.yaml ....

---
+++ app.py
@@ -0,0 +1,37 @@
+# [START catalog-reader-app]
+from flask import Flask
+import pymysql
+
+app = Flask('catalog-reader')
+
+connection = pymysql.connect(host='127.0.0.1',
+                             user='root',
+                             password='root',
+                             db='ase_assignment')
+
+requestBody='{"items": ['
+
+try:
+    with connection.cursor() as cursor:
+        # Read from database
+        sqlQuery = "SELECT item_name, price FROM Catalog"
+        cursor.execute(sqlQuery)
+        result = cursor.fetchall()
+
+        for row in result:
+         print(row)
+         requestBody += '{"itemName":"'+row[0]+'","itemPrice":'+ str(row[1]) +'},'
+finally:
+    connection.close()
+
+requestBody = requestBody[:-1]
+requestBody += ']}'
+
+
+@app.route('/')
+def hello():
+    return requestBody
+
+if __name__ == '__main__':
+  app.run(host = '0.0.0.0', port = 80)
+# [END catalog-reader-app]
