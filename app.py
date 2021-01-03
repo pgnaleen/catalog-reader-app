2cb6dad3cd20326e60da7f5f00dc9c1f58ff025c
Nalin Kularathna <pgnaleen@gmail.com>
2021-01-03 07:47:57 +0000

    db call inside request call

--- app.py
+++ app.py
@@ -29,26 +29,25 @@ connection = pymysql.connect(host='127.0.0.1',
 
 
 @app.route('/')
-
-requestBody='{"items": ['
-
-try:
-    with connection.cursor() as cursor:
-        # Read from database
-        sqlQuery = "SELECT item_name, price FROM Catalog"
-        cursor.execute(sqlQuery)
-        result = cursor.fetchall()
-
-        for row in result:
-         print(row)
-         requestBody += '{"itemName":"'+row[0]+'","itemPrice":'+ str(row[1]) +'},'
-finally:
-    connection.close()
-
-requestBody = requestBody[:-1]
-requestBody += ']}'
-
 def hello():
+    requestBody='{"items": ['
+
+    try:
+        with connection.cursor() as cursor:
+            # Read from database
+            sqlQuery = "SELECT item_name, price FROM Catalog"
+            cursor.execute(sqlQuery)
+            result = cursor.fetchall()
+
+            for row in result:
+                 print(row)
+                 requestBody += '{"itemName":"'+row[0]+'","itemPrice":'+ str(row[1]) +'},'
+    finally:
+        connection.close()
+
+    requestBody = requestBody[:-1]
+    requestBody += ']}'
+
     return requestBody
 
 if __name__ == '__main__':
