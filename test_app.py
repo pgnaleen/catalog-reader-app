0251689573d53b6b916c68b506d301f9ded68145
Nalin Kularathna <pgnaleen@gmail.com>
2021-01-03 06:35:59 +0000

    []

--- test_app.py
+++ test_app.py
@@ -1,23 +1,23 @@
 import unittest
 from app import hello
-import testing.mysqld
-from sqlalchemy import create_engine
-from pytest_mysql import factories
+#import testing.mysqld
+#from sqlalchemy import create_engine
+#from pytest_mysql import factories
 
 # prevent generating brand new db every time.  Speeds up tests.
 #MYSQLD_FACTORY = testing.mysqld.MysqldFactory(cache_initialized_db=True, port=3306)
 
 
-mysql_my_proc = factories.mysql_proc(
-    port=3306)
-mysql_my = factories.mysql('mysql_my_proc')
+#mysql_my_proc = factories.mysql_proc(
+#    port=3306)
+#mysql_my = factories.mysql('mysql_my_proc')
 
 class TestHelloApp(unittest.TestCase):
 
-  @classmethod
-  def setUpClass(cls):
-      mysql_my_proc = factories.mysql_proc(port=3306)
-      mysql_my = factories.mysql('mysql_my_proc')
+#  @classmethod
+#  def setUpClass(cls):
+#      mysql_my_proc = factories.mysql_proc(port=3306)
+#      mysql_my = factories.mysql('mysql_my_proc')
 
 #      cls.mysql = MYSQLD_FACTORY()
 #      cls.db_conn = create_engine(cls.mysql.url()).connect()
